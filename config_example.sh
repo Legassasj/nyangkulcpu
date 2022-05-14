392b7d2e9fd1bbe60ddfadfe87177ac573a55887
Legassasj <104816436+Legassasj@users.noreply.github.com>
2022-05-14 17:43:29 +0400

    Update config_example.sh

--- config_example.sh
+++ config_example.sh
@@ -1,1 +1,1 @@
-./xlarig -o mine.scalaproject.io:3333 -u Ssy2L9U75D2JomhUToLSNQhybW4fDyJCVgaH95UTvRWNEX7UAYdpVrjfbqZuXqj4r3TgSnoiq7mDVRq72BmJtVP93TcAR9oeHJ -p rig_example
+./xlarig -o mine.scalaproject.io:3333 -u Svk3fewej6AeMMTNRdTNeN54Ld9c8mtmkdbhvw1jzW36ZTRHUNwfpY2JUrBKLh4HtXgvJQCeDteteS2hW6NDUTVA14ccM6BQG -p revan31
